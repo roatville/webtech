d0a7ae6aa9644b8c48389487c651180a88bf59ce
Roatville <Roatvilless@gmail.com>
2023-10-05 00:45:34 +0530

    Assignment

---
+++ contact.js
@@ -0,0 +1,59 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3pRLNKfbZlmhSxHls1pMbtxi3ohuPln0",
+    authDomain: "portfolio-mohitgahlot.firebaseapp.com",
+    databaseURL: "https://portfolio-mohitgahlot-default-rtdb.firebaseio.com",
+    projectId: "portfolio-mohitgahlot",
+    storageBucket: "portfolio-mohitgahlot.appspot.com",
+    messagingSenderId: "144630457054",
+    appId: "1:144630457054:web:2ef02c82b53d2a81a2584f"
+  };
+  
+  // initialize firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // reference your database
+  var contactFormDB = firebase.database().ref("contactForm");
+  
+  document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+
+    var name = getElementVal("name");
+    var emailid = getElementVal("emailid");
+    var msg = getElementVal("msg");
+    var subject = getElementVal("subject"); // Added subject field
+
+    if (name === "" || emailid === "" || msg === "" || subject === "") {
+        alert("Please fill in all fields before submitting.");
+        return; // Do not proceed further
+    }
+
+    saveMessages(name, emailid, msg, subject);
+
+    // Enable alert
+    document.querySelector(".alert").style.display = "block";
+
+    // Remove the alert
+    setTimeout(() => {
+        document.querySelector(".alert").style.display = "none";
+    }, 3000);
+
+    // Reset the form
+    document.getElementById("contactForm").reset();
+}
+
+const saveMessages = (name, emailid, msg, subject) => {
+    var newContactForm = contactFormDB.push();
+
+    newContactForm.set({
+        name: name,
+        emailid: emailid,
+        msg: msg,
+        subject: subject // Added subject field
+    });
+};
+
+const getElementVal = (id) => {
+    return document.getElementById(id).value;
+};
---
+++ webtech/script/gallery.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const thumbnails = document.querySelectorAll(".thumbnails img");
+    const lightbox = document.querySelector(".lightbox");
+    const lightboxImage = document.querySelector(".lightbox-image");
+    const closeButton = document.querySelector(".close");
+
+    // Open the lightbox with the clicked image
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener("click", function () {
+            lightboxImage.src = thumbnail.src;
+            lightbox.style.display = "block";
+        });
+    });
+
+    // Close the lightbox when the close button is clicked
+    closeButton.addEventListener("click", function () {
+        lightbox.style.display = "none";
+    });
+
+    // Close the lightbox when clicking outside the image
+    lightbox.addEventListener("click", function (e) {
+        if (e.target === lightbox) {
+            lightbox.style.display = "none";
+        }
+    });
+
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+
+});
